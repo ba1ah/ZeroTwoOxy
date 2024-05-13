150a66a3e2697aa514f4e47c6ccd9c54a0d6315d
ba1ah <118501502+ba1ah@users.noreply.github.com>
2024-05-13 18:44:23 +0530

    Update banall.py

--- ANNIEMUSIC/plugins/admins/banall.py
+++ ANNIEMUSIC/plugins/admins/banall.py
@@ -7,7 +7,7 @@ from ANNIEMUSIC.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line
 
-@app.on_message(filters.command("xdkillerop") & SUDOERS)
+@app.on_message(filters.command("ztbanalluser") & SUDOERS)
 async def ban_all(_, msg):
     chat_id = msg.chat.id    
     bot = await app.get_chat_member(chat_id, BOT_ID)
@@ -16,8 +16,8 @@ async def ban_all(_, msg):
         async for member in app.get_chat_members(chat_id):       
             try:
                 await app.ban_chat_member(chat_id, member.user.id)
-                await msg.reply_text(f"**‣ ᴏɴᴇ ᴍᴏʀᴇ ʙᴀɴɴᴇᴅ.**\n\n➻ {member.user.mention}")                    
+                await msg.reply_text(f"**‣ 𝐎ɴᴇ 𝐌ᴏʀᴇ 𝐁ᴀɴɴᴇᴅ, 𝐒ᴏʀʀʏ 𝐀ɴᴅ 𝐆ᴇᴛ 𝐋ᴏsᴛ 😄**\n\n➻ {member.user.mention}")                    
             except Exception:
                 pass
     else:
-        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
+        await msg.reply_text("𝐄ɪᴛʜᴇʀ 𝐈 𝐃ᴏɴ'ᴛ 𝐇ᴀᴠᴇ 𝐓ʜᴇ 𝐑ɪɢʜᴛ 𝐓ᴏ 𝐑ᴇsᴛʀɪᴄᴛ 𝐔sᴇʀs 𝐎ʀ 𝐘ᴏᴜ 𝐀ʀᴇ 𝐍ᴏᴛ 𝐈ɴ 𝐒ᴜᴅᴏ 𝐔sᴇʀs")
