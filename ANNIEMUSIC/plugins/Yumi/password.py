faaef3859511f3ff782f0de948fcb5b5fde791a1
ba1ah <118501502+ba1ah@users.noreply.github.com>
2024-05-13 18:05:49 +0530

    Update password.py

--- ANNIEMUSIC/plugins/Yumi/password.py
+++ ANNIEMUSIC/plugins/Yumi/password.py
@@ -6,7 +6,7 @@ from config import BOT_USERNAME
 
 @app.on_message(filters.command(["genpassword", 'genpw']))
 async def password(bot, update):
-    message = await update.reply_text(text="Pʀᴏᴄᴇꜱꜱɪɴɢ..")
+    message = await update.reply_text(text="𝐏ʀᴏᴄᴇꜱꜱɪɴɢ....👀")
     password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
     if len(update.command) > 1:
         qw = update.text.split(" ", 1)[1]
@@ -16,5 +16,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url=f"https://t.me/{BOT_USERNAME}?startgroup=true")]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('🤌🏻 𝐀ᴅᴅ 𝐌ᴇ ❤️‍🔥', url=f"https://t.me/{BOT_USERNAME}?startgroup=true")]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
