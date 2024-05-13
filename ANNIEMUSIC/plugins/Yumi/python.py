7d219a2d25929237b465e758dbace1025c998e6a
ba1ah <118501502+ba1ah@users.noreply.github.com>
2024-05-13 18:15:56 +0530

    Update python.py

--- ANNIEMUSIC/plugins/Yumi/python.py
+++ ANNIEMUSIC/plugins/Yumi/python.py
@@ -9,7 +9,7 @@ from ANNIEMUSIC import app
 @app.on_message(filters.command("python"))
 async def execute_python_code(client, message: Message):
     if len(message.command) < 2:
-        await message.reply("Please enter your Python code after the command. Example: /python print('Hello, World!')")
+        await message.reply("𝐏ʟᴇᴀsᴇ 𝐄ɴᴛᴇʀ 𝐘ᴏᴜʀ 𝐏ʏᴛʜᴏɴ 𝐂ᴏᴅᴇ 𝐀ғᴛᴇʀ 𝐓ʜᴇ 𝐂ᴏᴍᴍᴀɴᴅ. 𝐄xᴀᴍᴘʟᴇ: /python print ('𝐇ᴇʟʟᴏ, 𝐖ᴏʀʟᴅ!')")
         return
 
     python_code = " ".join(message.command[1:])
@@ -17,8 +17,8 @@ async def execute_python_code(client, message: Message):
     try:
         # Execute the Python code
         exec_result = exec(python_code)
-        await message.reply(f"Code executed successfully. Result: {exec_result}")
+        await message.reply(f"𝐂ᴏᴅᴇ 𝐄xᴇᴄᴜᴛᴇᴅ 𝐒ᴜᴄᴄᴇssғᴜʟʟʏ. 𝐑ᴇsᴜʟᴛ: {exec_result}")
     except Exception as e:
         # Handle code execution errors
         traceback_str = traceback.format_exc()
-        await message.reply(f"Code execution error: {str(e)}\nTraceback:\n{traceback_str}")
+        await message.reply(f"𝐂ᴏᴅᴇ 𝐄xᴇᴄᴜᴛɪᴏɴ 𝐄ʀʀᴏʀ: {str(e)}\nTraceback:\n{traceback_str}")
