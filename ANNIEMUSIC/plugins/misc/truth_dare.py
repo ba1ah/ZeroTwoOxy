bab034aad6337c79bd6d54b040e883deb12d0b5e
ba1ah <118501502+ba1ah@users.noreply.github.com>
2024-05-13 20:44:15 +0530

    Update truth_dare.py

--- ANNIEMUSIC/plugins/misc/truth_dare.py
+++ ANNIEMUSIC/plugins/misc/truth_dare.py
@@ -15,11 +15,11 @@ def get_truth(client, message):
         response = requests.get(truth_api_url)
         if response.status_code == 200:
             truth_question = response.json()["question"]
-            message.reply_text(f"Truth question:\n\n{truth_question}")
+            message.reply_text(f"𝐓ʀᴜᴛʜ 𝐐ᴜᴇsᴛɪᴏɴ:\n\n{truth_question}")
         else:
-            message.reply_text("Failed to fetch a truth question. Please try again later.")
+            message.reply_text("𝐅ᴀɪʟᴇᴅ 𝐓ᴏ 𝐅ᴇᴛᴄʜ 𝐀 𝐓ʀᴜᴛʜ 𝐐ᴜᴇsᴛɪᴏɴ. 𝐏ʟᴇᴀsᴇ 𝐓ʀʏ 𝐀ɢᴀɪɴ 𝐋ᴀᴛᴇʀ.")
     except Exception as e:
-        message.reply_text("An error occurred while fetching a truth question. Please try again later.")
+        message.reply_text("𝐀ɴ 𝐄ʀʀᴏʀ 𝐎ᴄᴄᴜʀᴇᴅ 𝐖ʜɪʟᴇ 𝐅ᴇᴛᴄʜɪɴɢ 𝐀 𝐓ʀᴜᴛʜ 𝐐ᴜᴇsᴛɪᴏɴ. 𝐏ʟᴇᴀsᴇ 𝐓ʀʏ 𝐀ɢᴀɪɴ 𝐋ᴀᴛᴇʀ")
 
 @app.on_message(filters.command("dare"))
 def get_dare(client, message):
@@ -28,8 +28,8 @@ def get_dare(client, message):
         response = requests.get(dare_api_url)
         if response.status_code == 200:
             dare_question = response.json()["question"]
-            message.reply_text(f"Dare question:\n\n{dare_question}")
+            message.reply_text(f"𝐃ᴀʀᴇ 𝐐ᴜᴇsᴛɪᴏɴ:\n\n{dare_question}")
         else:
-            message.reply_text("Failed to fetch a dare question. Please try again later.")
+            message.reply_text("𝐅ᴀɪʟᴇᴅ 𝐓ᴏ 𝐅ᴇᴛᴄʜ 𝐀 𝐃ᴀʀᴇ 𝐐ᴜᴇsᴛɪᴏɴ. 𝐏ʟᴇᴀsᴇ 𝐓ʀʏ 𝐀ɢᴀɪɴ 𝐋ᴀᴛᴇʀ.")
     except Exception as e:
-        message.reply_text("An error occurred while fetching a dare question. Please try again later.")
+        message.reply_text("𝐀ɴ 𝐄ʀʀᴏʀ 𝐎ᴄᴄᴜʀᴇᴅ 𝐖ʜɪʟᴇ 𝐅ᴇᴛᴄʜɪɴɢ 𝐀 𝐃ᴀʀᴇ 𝐐ᴜᴇsᴛɪᴏɴ. 𝐏ʟᴇᴀsᴇ 𝐓ʀʏ 𝐀ɢᴀɪɴ 𝐋ᴀᴛᴇʀ")
