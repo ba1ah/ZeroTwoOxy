78a16c6f992a3ae1df9e5565bb225abfe8b0c281
ba1ah <118501502+ba1ah@users.noreply.github.com>
2024-05-13 18:27:57 +0530

    Update whatsApp.py

--- ANNIEMUSIC/plugins/Yumi/whatsApp.py
+++ ANNIEMUSIC/plugins/Yumi/whatsApp.py
@@ -6,7 +6,7 @@ from ANNIEMUSIC import app
 @app.on_message(filters.command("WhatsApp"))
 async def generate_whatsapp_link(client, message: Message):
     if len(message.command) < 2:
-        await message.reply("Please enter your phone number after the command. Example: /WhatsApp +1234567890")
+        await message.reply("𝐏ʟᴇᴀsᴇ enter your phone number after the command. Example: /WhatsApp +1234567890")
         return
 
     phone_number = message.command[1]
@@ -14,4 +14,4 @@ async def generate_whatsapp_link(client, message: Message):
     # Generate the WhatsApp link
     whatsapp_link = f"https://wa.me/{phone_number}"
 
-    await message.reply(f"Click the link to open WhatsApp with the number {phone_number}:\n{whatsapp_link}")
+    await message.reply(f"𝐂ʟɪᴄᴋ 𝐓ʜᴇ 𝐋ɪɴᴋ 𝐓ᴏ 𝐎ᴘᴇɴ 𝐖ʜᴀᴛs𝐀ᴘᴘ 𝐖ɪᴛʜ 𝐓ʜᴇ 𝐍ᴜᴍʙᴇʀ {phone_number}:\n{whatsapp_link}")
